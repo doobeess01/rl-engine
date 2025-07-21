f809685ef18b0f065d43e63537925bc411dbc180
David Dee <david.m.dee01@gmail.com>
2025-07-21 08:45:46 -0500

    Added wall-checks to Wander controller

--- game/controllers.py
+++ game/controllers.py
@@ -1,11 +1,19 @@
 import random
 
 from game.controller import Controller
-from game.actions import Bump
+from game.actions import Bump, Wait
+from game.tiles import TILES
+from game.components import Position, Tiles
+from game.tags import IsIn
+
 
 class Wander(Controller):
     def __call__(self, actor):
-        d = (0,0)
-        while d == (0,0):
-            d = (random.randint(-1,1), random.randint(-1,1))
-        return Bump(d)
+        directions = [(0,1),(1,0),(1,1),(0,-1),(-1,0),(-1,-1),(-1,1),(1,-1)]
+        map_ = actor.relation_tag[IsIn]
+        while directions:
+            direction = random.choice(directions)
+            new_position = actor.components[Position] + direction
+            if TILES['walk_cost'][map_.components[Tiles][new_position.ij]]>0:
+                return Bump(direction)
+        return Wait()
